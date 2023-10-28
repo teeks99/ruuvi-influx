f7b5a46dae256ad45c80fa9f245f2d2a6c8a9a19
Tom Kent <tom@teeks99.com>
2023-10-28 16:21:52 -0500

    Store when changing and notification

--- influx_service.py
+++ influx_service.py
@@ -1,4 +1,7 @@
 import argparse
+import datetime
+import socket
+import urllib.request
 
 import os
 os.environ["RUUVI_BLE_ADAPTER"] = "bleak"
@@ -22,19 +25,75 @@ def parse_cli_args():
     parser.add_argument(
         '--influx-bucket', default="ruuvi",
         help="Influxdb bucket to store records in")
+    parser.add_argument(
+        "--max-interval-sec", default="0",
+        help="If this is set, only updates that have changed will be stored " +
+        "or a minimum of this many seconds")
+    parser.add_argument(
+        "--check-url", default="",
+        help="URL to notify each time that a packet is sent.")
 
     return parser.parse_args()
 
 class DataChecker(object):
-    def __init__(self, influx_db):
+    def __init__(self, influx_db, max_interval_sec=0, check_url=None):
         self.influx_db = influx_db
+
+        self.max_interval_sec = max_interval_sec
+        self.last_stored = {}
+
+        self.check_url = check_url
 
     async def main(self):
         async for data in RuuviTagSensor.get_data_async():
             print(data)
-            mac = data[0]
-            payload = data[1]
-            self.influx_db.write_ruuvi_record(mac, payload)
+            self.mac = data[0]
+            self.payload = data[1]
+
+            if self.check_for_send():
+                self.influx_db.write_ruuvi_record(self.mac, self.payload)
+                self.notify_url()
+
+    def check_for_send(self):
+        self.trimmed_payload = self.payload.copy()
+        del self.trimmed_payload["measurement_sequence_number"]
+
+        if not self.max_interval_sec:
+            self.store()
+            return True
+
+        if self.ready():
+            self.store()
+            return True
+
+        return False
+
+    def store(self):
+        self.last_stored[self.mac] = {
+            "payload": self.trimmed_payload, 
+            "last_time": datetime.datetime.utcnow()
+        }
+
+    def ready(self):
+        if not self.mac in self.last_stored:
+            return True
+        
+        if self.trimmed_payload != self.last_stored[self.mac]["payload"]:
+            return True
+
+        last_time = self.last_stored[self.mac]["last_time"]
+        if datetime.datetime.utcnow() - last_time > self.max_interval_sec:
+            return True
+
+        return False
+
+    def notify_url(self):
+        if self.check_url:
+            try:
+                urllib.request.urlopen(self.check_url, timeout=10)
+            except socket.error as e:
+                print(f"Check URL call failed: {e}")
+
 
 if __name__ == "__main__":
     args = parse_cli_args()
@@ -42,7 +101,7 @@ if __name__ == "__main__":
     db = influx_writer.RuuviInfluxWriter(
         args.influx_url, args.influx_org, args.influx_bucket)
     
-    dc = DataChecker(db)
+    dc = DataChecker(db, int(args.max_interval_sec), args.check_url)
 
     #RuuviTagSensor.get_data(write_to_influxdb)
     asyncio.get_event_loop().run_until_complete(dc.main())
